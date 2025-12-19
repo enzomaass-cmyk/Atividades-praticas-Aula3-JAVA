028a4d5d6e7f78c8b861eee738fbba72fb3dfb05
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-12-19 10:26:44 -0300

    exercício1 com todos os testes

---
+++ IFSimples.java
@@ -0,0 +1,21 @@
+
+public class IFSimples {
+    public static void main(String[] args) {
+        int idade = 65;
+
+        // Verifique se é maior de idade
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade");
+        }
+
+        // Verifique se pode votar
+        if (idade >= 16) {
+            System.out.println("Você pode votar");
+        }
+
+        // Verifique se é idoso
+        if (idade >= 60) {
+            System.out.println("Você é idoso");
+        }
+    }
+}
